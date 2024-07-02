0a7a1cbee0f4c53a48788f834f9283683b7e3650
Aaron <aaronsmitstc@gmail.com>
2024-07-02 18:05:01 -0500

    update layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,31 @@
-<script>
-    import "$lib/styles/app.css";
-</script>
+<slot />
 
-<div class="content">
-    <slot />
-</div>
+<style>
+    :global(body) {
+        background: linear-gradient(90deg, #f5e9d7, #fedeff);
+        background-size: 1000% 1000%;
+        animation: gradientAnimation 20s ease infinite;
+        margin: 0 auto;
+        width: 100%;
+        min-height: 100vh;
+        font-family: Arial, sans-serif;
+    }
+
+    @keyframes gradientAnimation {
+        0% {
+            background-position: 0% 50%;
+        }
+
+        50% {
+            background-position: 100% 50%;
+        }
+
+        100% {
+            background-position: 0% 50%;
+        }
+    }
+
+
+
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -27,21 +27,86 @@
 
     return () => clearInterval(interval);
   });
+
+  let name = '';
+  let email = '';
+
+  function handleSubmit() {
+    // Handle form submission
+    console.log('Name:', name);
+    console.log('Email:', email);
+    // Reset form fields
+    name = '';
+    email = '';
+  }
 </script>
 
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 400">
-  <rect width="100%" height="100%" fill="#f5e9d7" rx="15"/>
-  
-  {#each colorSequences as sequence, rowIndex}
-    <g transform="translate(0, {rowIndex * 50})">
-      {#each [25, 75, 125, 175, 225, 275] as cx, index}
-        <circle {cx} cy="25" r="20">
-          <animate attributeName="fill" values={sequence} dur="2s" repeatCount="indefinite" begin={`${index * 0.5}s`}/>
-        </circle>
-      {/each}
-    </g>
-  {/each}
-  
-  <text x="20" y="380" font-family="Arial, sans-serif" font-size="24" font-weight="bold" textLength="50%">OPEN MIC</text>
-  <text x="220" y="380" font-family="Arial, sans-serif" font-size="24" font-weight="bold" textLength="20%">08/28</text>
-</svg>
+<div class="container">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 400">   
+    {#each colorSequences as sequence, rowIndex}
+      <g transform="translate(0, {rowIndex * 50})">
+        {#each [25, 75, 125, 175, 225, 275] as cx, index}
+          <circle {cx} cy="25" r="20">
+            <animate attributeName="fill" values={sequence} dur="2s" repeatCount="indefinite" begin={`${index * 0.5}s`}/>
+          </circle>
+        {/each}
+      </g>
+    {/each}
+    
+    <text x="0" y="380" font-family="Arial, sans-serif" font-size="24" font-weight="bold" textLength="180px">OPEN MIC</text>
+    <text x="220" y="380" font-family="Arial, sans-serif" font-size="24" font-weight="bold" textLength="80px">08/28</text>
+  </svg>
+
+  <div class="form-container">
+    <form on:submit|preventDefault={handleSubmit}>
+      <input type="text" id="name" placeholder="NAME" bind:value={name} required>
+      <input type="email" placeholder="EMAIL" id="email" bind:value={email} required>
+      <button type="submit">SIGN UP</button>
+    </form>
+  </div>
+</div>
+
+<style>
+  .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    max-width: 800px;
+    max-height: 100vh;
+    margin: 0 auto;
+  }
+
+  .form-container {
+    margin: 20px;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+  }
+
+  input {
+    margin-bottom: 10px;
+    padding-left: 100px;
+    padding-right: 100px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    border-radius: 10px;
+    text-align: center;
+    background-color: inherit;
+    border-width: 1px;
+    border-color: black;
+
+  }
+
+  button {
+    padding: 10px;
+    border-radius: 10px;
+    font-weight: bold;
+    background: inherit;
+    border-width: 1px;
+    border-color: black;
+    letter-spacing: 4px;
+  }
+</style>
